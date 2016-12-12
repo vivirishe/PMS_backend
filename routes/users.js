413bcbc6b1158f96aff7acfa25db5454d2ab1423
viviana rishe <vivianacuri@gmail.com>
2016-12-11 21:01:46 -0800

    brings auth token to users routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/users')
+var token = require('../config/token_auth')
 
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
